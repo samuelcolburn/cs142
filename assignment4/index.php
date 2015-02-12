61dd14e21fc637f19458b61329e7b9a1ad776ce4
Samuel Colburn <samuel.colburn@uvm.edu>
2015-02-12 00:07:10 -0500

    update
    -added base course style

---
+++ assignment4/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+
+
+        <title>Assignment 4 Index</title>
+    </head>
+    <body>
+        <h1>Assignment 4 Index</h1>
+        <article>
+
+            <ul>
+                <li>Assignment 4: Redesign the Course Style <a href="assignment4/index.php">Index </a> - <a href="assignment4/smcolbur.css">Style Sheet</a>- <a href="assignment4/print_smcolbur.css">Print Style Sheet</a></li>
+
+            </ul>
+        </article>
+    </body>
+</html>
